f6976646684c508f0636b08c506050cc9bb4e917
Yapichoy <torezracer@gmail.com>
2020-10-28 13:07:01 +0300

    test

--- app/router/router.js
+++ app/router/router.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 router.get('/', (req, res) => {
     console.log(__dirname);
-    res.sendFile(path.join(__dirname, "../../build/index.html"));
+    res.sendFile(path.join(__dirname, "../build/index.html"));
 })
 
 router.get('/about', function(req, res) {
